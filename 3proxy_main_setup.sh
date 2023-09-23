888e513c10a2bec3d785f59a4333e0b799d56c71
kypc86 <kypc86@gmail.com>
2023-09-23 18:26:18 +0700

    Update 3proxy_main_setup.sh

--- 3proxy_main_setup.sh
+++ 3proxy_main_setup.sh
@@ -17,12 +17,15 @@ install_3proxy() {
     URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.4
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/init.d/3proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
+    ln -s Makefile.Linux Makefile
+    make
+    sudo make install
+    # make -f Makefile.Linux
+    # mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    # cp src/3proxy /usr/local/etc/3proxy/bin/
+    # cp ./scripts/init.d/3proxy.sh /etc/init.d/3proxy
+    # chmod +x /etc/init.d/3proxy
+    # chkconfig 3proxy on
     cd $WORKDIR
 }
 
